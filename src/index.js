157359ca5bf6515ec7fd3481b1ca04e253e8a72b
Kyle Mark Boomer <kylemboomer@gmail.com>
2024-10-05 09:40:04 -0500

    Refactor index.js, App.js for proper imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
