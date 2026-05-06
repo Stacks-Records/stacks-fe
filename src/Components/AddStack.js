ba1f9caba48346b839ea47b63935d66a38f7d185
Kyle M Boomer <kyle.boomer@smartwayslfl.com>
2026-05-06 12:06:40 -0500

    feat: refactored AddStack for edit mode

--- src/Components/AddStack.js
+++ src/Components/AddStack.js
@@ -1,18 +1,17 @@
 import { useContext, useState, useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { getRecords, postAlbum } from './APICalls'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { getRecords, postAlbum, editAlbum } from './APICalls'
 import '../CSS/AddStack.css'
-import { useAuth0 } from '@auth0/auth0-react'
 import AuthAlbumContext from '../Context/AuthAlbumContext'
 
 function AddStack() {
     const formRef = useRef()
-    const [successtext, setSuccessText] = useState('')
-    const { albums, setAlbums } = useContext(AuthAlbumContext)
-    const { authCode } = useContext(AuthAlbumContext)
+    const { setAlbums, authCode } = useContext(AuthAlbumContext)
     const [error, setError] = useState('')
     const navigate = useNavigate()
-
+    const location = useLocation()
+    const albumToEdit = location.state?.albumToEdit ?? null
+    const isEditMode = Boolean(albumToEdit)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -22,84 +21,73 @@ function AddStack() {
         album.bandMembers = formData.getAll('bandMembers')
 
         try {
-            console.log('submitting album:', album)
-            const result = await postAlbum(authCode, album)
-            const getAlbums = async () => {
-                try {
-                    const updatedAlbums = await getRecords(authCode)
-                    setAlbums(updatedAlbums)
-                }
-                catch (error) {
-                    console.log(error)
-                }
+            if (isEditMode) {
+                await editAlbum(albumToEdit.id, album, authCode)
+            } else {
+                await postAlbum(authCode, album)
             }
-            getAlbums()
-            navigate('/landing')
+            const updatedAlbums = await getRecords(authCode)
+            setAlbums(updatedAlbums)
+            navigate(isEditMode ? `/${albumToEdit.id}` : '/landing')
         } catch (error) {
             console.error('Error:', error.message)
-            setError('Failed to add album. Please try again.')
+            setError(isEditMode ? 'Failed to update album. Please try again.' : 'Failed to add album. Please try again.')
         }
     }
 
-    return ( 
+    return (
         <div className="add-stack">
             <div className='h-text-wrapper'>
-
-                <h1>Add Album to Stacks</h1>
-
+                <h1>{isEditMode ? 'Edit Album' : 'Add Album to Stacks'}</h1>
             </div>
-        <form ref={formRef} onSubmit={handleSubmit} className='add-stack-form'>
-
-            <div className="input-group">
-                <label><sub>*</sub>Album Name</label>
-                <input required type="text" name="albumName" placeholder="Album Name" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Artist</label>
-                <input required type="text" name="artist" placeholder="Artist" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Release Date</label>
-                <input required type="date" name="releaseDate" placeholder="Release Date" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Genre</label>
-                <input required type="text" name="genre" placeholder="Genre" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Label</label>
-                <input required type="text" name="label" placeholder="Label" />
-            </div>
-          
-            <div className="input-group">
-                <label><sub>*</sub>Rolling Stone Review</label>
-                <input required type="text" name="rollingStoneReview" placeholder="* / ** / *** / **** / *****" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Youtube Album URL</label>
-                <input required type="url" name="youTubeAlbumURL" placeholder="YouTube Album URL" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Image URL</label>
-                <input required type="url" name="imgURL" placeholder="Image URL" />
-            </div>
-            <div className="input-group">
-                <label><sub>*</sub>Albums Sold</label>
-                <input required type="number" name="albumsSold" placeholder="Albums Sold" />
-            </div>
-            <div className='input-group'>
-                <label><sub>*</sub>Band Members</label>
-                <input type="text" name="bandMembers" placeholder="Band Members" />
-            </div>
-            <div className='input-group'>
-                <label>
-                <sub>*</sub>Are they still together?
-                </label>
-                <input type="checkbox" name="isBandTogether" />
-            </div>
-            <button type="submit">Add Album</button>
-            {error && <p className="error">{error}</p>}
-            <div className='required-text'><sub>*</sub>Indicates required field</div>
+            <form ref={formRef} onSubmit={handleSubmit} className='add-stack-form'>
+                <div className="input-group">
+                    <label><sub>*</sub>Album Name</label>
+                    <input required type="text" name="albumName" placeholder="Album Name" defaultValue={albumToEdit?.albumName ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Artist</label>
+                    <input required type="text" name="artist" placeholder="Artist" defaultValue={albumToEdit?.artist ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Release Date</label>
+                    <input required type="date" name="releaseDate" placeholder="Release Date" defaultValue={albumToEdit?.releaseDate ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Genre</label>
+                    <input required type="text" name="genre" placeholder="Genre" defaultValue={albumToEdit?.genre ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Label</label>
+                    <input required type="text" name="label" placeholder="Label" defaultValue={albumToEdit?.label ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Rolling Stone Review</label>
+                    <input required type="text" name="rollingStoneReview" placeholder="* / ** / *** / **** / *****" defaultValue={albumToEdit?.rollingStoneReview ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Youtube Album URL</label>
+                    <input required type="url" name="youTubeAlbumURL" placeholder="YouTube Album URL" defaultValue={albumToEdit?.youTubeAlbumURL ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Image URL</label>
+                    <input required type="url" name="imgURL" placeholder="Image URL" defaultValue={albumToEdit?.imgURL ?? ''} />
+                </div>
+                <div className="input-group">
+                    <label><sub>*</sub>Albums Sold</label>
+                    <input required type="number" name="albumsSold" placeholder="Albums Sold" defaultValue={albumToEdit?.albumsSold ?? ''} />
+                </div>
+                <div className='input-group'>
+                    <label><sub>*</sub>Band Members</label>
+                    <input type="text" name="bandMembers" placeholder="Band Members" defaultValue={albumToEdit?.bandMembers ?? ''} />
+                </div>
+                <div className='input-group'>
+                    <label><sub>*</sub>Are they still together?</label>
+                    <input type="checkbox" name="isBandTogether" defaultChecked={albumToEdit?.isBandTogether ?? false} />
+                </div>
+                <button type="submit">{isEditMode ? 'Save Changes' : 'Add Album'}</button>
+                {error && <p className="error">{error}</p>}
+                <div className='required-text'><sub>*</sub>Indicates required field</div>
             </form>
         </div>
     )
