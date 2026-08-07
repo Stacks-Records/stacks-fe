74cc21ec77b99b3f2b465e1a59b264925bc7c878
Kyle M. Boomer <kylemboomer@gmail.com>
2026-08-07 14:06:28 -0500

    fix: BASE_URL in APICalls

--- src/Components/APICalls.js
+++ src/Components/APICalls.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001' || process.env.REACT_APP_API_URL
+const BASE_URL = process.env.REACT_APP_API_URL
 // const BASE_URL = 'http://localhost:3001'
 
 export const getUserRole = async (email, token) => {
