cfb7375ededb0351ccec05f8710ee717f1a6f90c
Kyle M. Boomer <kylemboomer@gmail.com>
2026-08-13 15:04:27 -0500

    feat: cypress update and config

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,14 @@
 const { defineConfig } = require("cypress");
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
     },
   },
+
   env: {
     auth0_username: process.env.REACT_APP_AUTH0_USERNAME,
     auth0_password: process.env.REACT_APP_AUTH0_PASSWORD,
@@ -18,5 +18,13 @@ module.exports = defineConfig({
     auth0_client_id: process.env.REACT_APP_CLIENT_ID,
     auth0_client_secret: process.env.REACT_APP_CLIENT_SECRET,
   },
-   chromeWebSecurity: false
+
+  chromeWebSecurity: false,
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
 });
